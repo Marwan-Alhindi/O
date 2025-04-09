8abd2a12a8291a2a5f58094e73cce30c126cf020
Marwan Alhindi <mfhmarwin@gmail.com>
2025-04-09 15:29:52 +0300

    added a button to expand message in Message.jsx!

--- langpulse.client/src/app/components/Message.jsx
+++ langpulse.client/src/app/components/Message.jsx
@@ -2,15 +2,30 @@ import { useState } from "react"
 
 function Message ({text}) {
     const [msgLen, setMsgLen] = useState()
-
+    const [more, setIsMore] = useState(true)    
     function determineBtnRender () {
         
         return Boolean
     }
     return (
-        <div className="bg-neutral-800 border border-yellow-300 border-solid mt-4 rounded-lg max-h-160 max-w-90">
-            <p className="m-4 line-clamp-25">{text}</p>
+        <div className="bg-neutral-800 border border-yellow-300 border-solid mt-4 rounded-lg max-h-160 max-w-90 flex flex-col justify-between">
 
+            {more ? (
+                <div>
+                    <p className="m-4 line-clamp-25">{text}</p>
+                    <div className="flex justify-end mt-4">
+                        <button
+                        className="border border-yellow-300 border-solid p-2"
+                        onClick={() => setIsMore(!more)}
+                        ></button>
+                    </div>
+                </div>
+
+            ) : (
+                <div>
+                    <p>{text}</p>
+                </div>
+            )}
         </div>
     )
 }
