b3b28f836e3454a73066829167d18ae753b7e9b7
Marwan Alhindi <mfhmarwin@gmail.com>
2025-04-11 17:56:18 +0300

    fixed rendering messages using setAIMessages instead of updating it using map. + pushed the box to the right

---
+++ langpulse.client/src/app/components/AIMessage.jsx
@@ -0,0 +1,12 @@
+
+function AIMessage ({text}) {
+    return (
+        <div className="border border-white border-dashed">
+            <p>{text}</p>
+        </div>
+
+    )
+}
+
+
+export default AIMessage
--- langpulse.client/src/app/components/Chat.jsx
+++ langpulse.client/src/app/components/Chat.jsx
@@ -1,5 +1,8 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Message from "./Message"
+import AIMessage from "./AIMessage"
+import React from "react"
+
 function Chat () {
     const [userMessages, setUserMessages] = useState([])
     const [inputText, setInputText] = useState("")
@@ -17,8 +20,7 @@ function Chat () {
         fetch(`http://localhost:8000/openai?user_input=${inputText}`)
         .then(res => res.json())
         .then(data => {
-            answerMessages.push(data)
-            console.log(answerMessages)
+            setAnswerMessages(prev => [...prev, data])
         })
         .catch(err => {
             console.error("Fetch error:", err);
@@ -28,70 +30,78 @@ function Chat () {
     return (
         <div className="flex-grow m-4 ml-0 p-6 bg-zinc-900 rounded-2xl border border-neutral-700 shadow-inner text-white">
             {userMessages.length > 0 ? (
-            <div>
-                {/* Top actions */}
-                <div className="flex flex-row justify-end items-center gap-x-2">
+                <div>
+                    {/* Top actions */}
+                    <div className="flex flex-row justify-end items-center gap-x-2">
+                        <button><img src='public/LLMinvite.png' /></button>
+                        <button><img src='public/userInvite.png' width={30} height={30}/></button>
+                        <button><img src='public/searchBar.png' width={30} height={30}/></button>
+                        <button><img src='public/info.png' width={30} height={30}/></button>
+                    </div>
+
+                    <div className="max-h-[700px] overflow-y-auto flex flex-col gap-2 pr-2 text-white">
+                        {userMessages.map((message, i) => {
+                            const AImessage = answerMessages[i];
+                            return (
+                                <React.Fragment key={i}>
+                                <div className="flex left-0 justify-end mr-6">
+                                    <Message text={message} />
+                                </div>
+                                <div>
+                                    <AIMessage text={AImessage} />
+                                </div>
+                                </React.Fragment>
+                            );
+                        })}
+                    </div>
+                    {/* Input field */}
+                    <div className="absolute bottom-20 right-80 w-full max-w-xl border border-yellow-500 rounded-xl px-4 py-3 flex items-center gap-2">
+                        <input
+                        type="text"
+                        placeholder="Ask anything..."
+                        className="bg-transparent outline-none flex-grow text-white placeholder-neutral-400"
+                        value={inputText}
+                        onChange={(e) => setInputText(e.target.value)}
+                        />
+                        <button className="text-yellow-400 hover:text-yellow-300">
+                        <img src="public/attachFile.png" height={30} width={30} />
+                        </button>
+                        <button className="text-yellow-400 hover:text-yellow-300" onClick={() => {handleInputMessages(); handleAIResponse(inputText);} }>
+                        <img src="public/sendMessage.png" height={30} width={30} />
+                        </button>
+                    </div>
+                </div>
+                ) : (
+                <>
+                    <div className="flex flex-row justify-end items-center gap-x-2">
                     <button><img src='public/LLMinvite.png' /></button>
                     <button><img src='public/userInvite.png' width={30} height={30}/></button>
                     <button><img src='public/searchBar.png' width={30} height={30}/></button>
-                    <button><img src='public/info.png' width={30} height={30}/></button>
-                </div>
+                    <button><img src='public/info.png' width={30} height={30} /></button>
+                    </div>
 
-                {/* messages section */}
-                <div className="max-h-[700px] overflow-y-auto flex flex-col gap-2 pr-2">
-                    {userMessages.map((message, i) => (
-                        <div key={i} className="">
-                        <Message text={message} />
-                        </div>
-                    ))}
-                </div>
-                {/* Input field */}
-                <div className="absolute bottom-20 right-80 w-full max-w-xl border border-yellow-500 rounded-xl px-4 py-3 flex items-center gap-2">
-                    <input
-                    type="text"
-                    placeholder="Ask anything..."
-                    className="bg-transparent outline-none flex-grow text-white placeholder-neutral-400"
-                    value={inputText}
-                    onChange={(e) => setInputText(e.target.value)}
-                    />
-                    <button className="text-yellow-400 hover:text-yellow-300">
-                    <img src="public/attachFile.png" height={30} width={30} />
-                    </button>
-                    <button className="text-yellow-400 hover:text-yellow-300" onClick={() => {handleInputMessages(); handleAIResponse(inputText);} }>
-                    <img src="public/sendMessage.png" height={30} width={30} />
-                    </button>
-                </div>
-            </div>
-            ) : (
-            <>
-                <div className="flex flex-row justify-end items-center gap-x-2">
-                <button><img src='public/LLMinvite.png' /></button>
-                <button><img src='public/userInvite.png' width={30} height={30}/></button>
-                <button><img src='public/searchBar.png' width={30} height={30}/></button>
-                <button><img src='public/info.png' width={30} height={30} /></button>
-                </div>
+                    <div className="flex flex-col h-full w-full items-center justify-center">
+                    <p className="text-lg mb-6">What do you want to work on?</p>
 
-                <div className="flex flex-col h-full w-full items-center justify-center">
-                <p className="text-lg mb-6">What do you want to work on?</p>
-
-                <div className="w-full max-w-xl border border-yellow-500 rounded-xl px-4 py-3 flex items-center gap-2">
-                    <input
-                    type="text"
-                    placeholder="Ask anything..."
-                    className="bg-transparent outline-none flex-grow text-white placeholder-neutral-400"
-                    value={inputText}
-                    onChange={(e) => setInputText(e.target.value)}
-                    />
-                    <button className="text-yellow-400 hover:text-yellow-300">
-                    <img src="public/attachFile.png" height={30} width={30} />
-                    </button>
-                    <button className="text-yellow-400 hover:text-yellow-300" onClick={() => {handleInputMessages(); handleAIResponse(inputText);}}>
-                    <img src="public/sendMessage.png" height={30} width={30} />
-                    </button>
-                </div>
-                </div>
-            </>
-            )}
+                    <div className="w-full max-w-xl border border-yellow-500 rounded-xl px-4 py-3 flex items-center gap-2">
+                        <input
+                        type="text"
+                        placeholder="Ask anything..."
+                        className="bg-transparent outline-none flex-grow text-white placeholder-neutral-400"
+                        value={inputText}
+                        onChange={(e) => setInputText(e.target.value)}
+                        />
+                        <button className="text-yellow-400 hover:text-yellow-300">
+                        <img src="public/attachFile.png" height={30} width={30} />
+                        </button>
+                        <button className="text-yellow-400 hover:text-yellow-300" onClick={() => {handleInputMessages(); handleAIResponse(inputText);}}>
+                        <img src="public/sendMessage.png" height={30} width={30} />
+                        </button>
+                    </div>
+                    </div>
+                </>
+                )
+            }
         </div>
     )
 }
